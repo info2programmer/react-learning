2f5c7b05934c60918771ca326d53f953986cd6da
Saikat Bhadury <info2programmer@gmail.com>
2019-03-12 13:53:31 +0530

    Render List Dynamic

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,31 +3,22 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
-  render() {
+
+  rendarTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags found</p>;
+
     return (
-      <div>
-        <span className={this.getCounterClasses()}>{this.formatCounter()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
-
-  getCounterClasses() {
-    let badgeIndicator = "m-2 badge badge-";
-    badgeIndicator += this.state.count === 0 ? "warning" : "primary";
-    return badgeIndicator;
-  }
-
-  formatCounter() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return <div>{this.rendarTags()}</div>;
   }
 }
 
