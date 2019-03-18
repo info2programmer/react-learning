ed8c50abf282e7930aab43a5622a69798e0822c1
Saikat Bhadury <info2programmer@gmail.com>
2019-03-18 13:53:50 +0530

    Nav bar implemnet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 2 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 3 }
+    ]
+  };
+
+  // This Function For Reset
+  handelReset = () => {
+    let counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  };
+
+  // Delete Counter Methord
+  handelDelete = counterId => {
+    let counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  // This Function For Handel Incrment Each Count
+  handelIncriment = counter => {
+    let counters = [...this.state.counters];
+    let index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    //   this.setState({counters})
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <NavBar
+          totalCounter={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onReset={this.handelReset}
+            onIncrement={this.handelIncriment}
+            onDelete={this.handelDelete}
+            counters={this.state.counters}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  
   render() {
     return (
       <div>
         <span className={this.getCounterClasses()}>{this.formatCounter()}</span>
-        <button onClick={() => this.props.onIncriment(this.props.counter)}  className="btn btn-secondary btn-sm"
+        <button
+          onClick={() => this.props.onIncriment(this.props.counter)}
+          className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <button className="btn-danger btn btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
+        <button
+          className="btn-danger btn btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
       </div>
     );
   }
@@ -23,7 +29,7 @@ class Counter extends Component {
 
   formatCounter() {
     const { value: count } = this.props.counter;
-    return count === 0 ? "Zero" : count;
+    return count === 0 ? "0" : count;
   }
 }
 
